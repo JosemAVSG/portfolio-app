305cd0f9899c63d94f494d151a07982ddc252e37
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-08-02 23:59:50 -0500

    portofolionext init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <h1>Navbar</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
